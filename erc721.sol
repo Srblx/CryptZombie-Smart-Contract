d1e095a28c8c3c001733ee34e9166dac828779d1
SXBL <122864409+Srblx@users.noreply.github.com>
2023-05-09 15:04:08 +0200

    Update erc721.sol

--- erc721.sol
+++ erc721.sol
@@ -1,10 +1,23 @@
+// Contrat ERC721 pour les tokens non fongibles
 contract ERC721 {
+  // Événement émis lors d'un transfert de token
   event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
+
+  // Événement émis lors d'une approbation de transfert de token
   event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
 
+  // Retourne le solde de tokens d'un propriétaire
   function balanceOf(address _owner) public view returns (uint256 _balance);
+
+  // Retourne l'adresse du propriétaire d'un token donné
   function ownerOf(uint256 _tokenId) public view returns (address _owner);
+
+  // Transfère un token à une adresse spécifique
   function transfer(address _to, uint256 _tokenId) public;
+
+  // Approuve une adresse pour transférer un token donné
   function approve(address _to, uint256 _tokenId) public;
+
+  // Effectue le transfert d'un token approuvé à l'adresse du preneur
   function takeOwnership(uint256 _tokenId) public;
 }

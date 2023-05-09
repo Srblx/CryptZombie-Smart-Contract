d3c8ce9ca5c50af79fb6ee0641b21ddfa2358cb3
SXBL <122864409+Srblx@users.noreply.github.com>
2023-05-09 15:07:48 +0200

    Update zombiefeeding.sol

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,7 +1,9 @@
+// Version du compilateur Solidity
 pragma solidity ^0.8.9;
 
 import "./zombiefactory.sol";
 
+// Interface du contrat Kitty
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
@@ -21,35 +23,41 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
+  // Modificateur qui vérifie que l'appelant est le propriétaire du zombie
   modifier onlyOwnerOf(uint _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     _;
   }
 
+  // Définir l'adresse du contrat Kitty
   function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
   }
 
+  // Déclencher le temps de récupération du zombie
   function _triggerCooldown(Zombie storage _zombie) internal {
     _zombie.readyTime = uint32(now + cooldownTime);
   }
 
+  // Vérifier si le zombie est prêt pour l'action
   function _isReady(Zombie storage _zombie) internal view returns (bool) {
       return (_zombie.readyTime <= now);
   }
 
+  // Nourrir le zombie avec un ADN cible et le multiplier
   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    if (keccak256(_species) == keccak256("kitty")) {
+    if (keccak256(bytes(_species)) == keccak256("kitty")) {
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
     _triggerCooldown(myZombie);
   }
 
+  // Nourrir le zombie avec un chaton
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
